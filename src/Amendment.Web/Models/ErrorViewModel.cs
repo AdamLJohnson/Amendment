de8801c3b620f2fe3ad3ccce027a1e6d03ba91eb
Adam Johnson <columbus@adamlj.com>
2018-08-01 20:29:55 -0700

    Initial commit of major pieces

---
+++ src/Amendment.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Amendment.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
