406e5605ff577722a9bb21bed90b83ce7369382d
Adam Johnson <columbus@adamlj.com>
2018-08-04 16:36:23 -0700

    Adds the Admin area

--- src/Amendment.Web/Startup.cs
+++ src/Amendment.Web/Startup.cs
@@ -80,6 +80,11 @@ namespace Amendment.Web
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
