54a72bbf8b83b587039ded56c4ea00e48b5f623c
Adam Johnson <columbus@adamlj.com>
2019-04-10 22:10:09 -0500

    Adds scope ID to the log

--- src/Amendment.Web/Controllers/HomeController.cs
+++ src/Amendment.Web/Controllers/HomeController.cs
@@ -72,5 +72,7 @@ namespace Amendment.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Throw() => throw new Exception("AHHHH");
     }
 }
