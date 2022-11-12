d2fc253d26dc873b7e8d58a8dc26f95411726eee
Adam Johnson <columbus@adamlj.com>
2022-11-11 18:24:42 -0800

    Using Mapster

--- src/Amendment.Server/Controllers/AccountController.cs
+++ src/Amendment.Server/Controllers/AccountController.cs
@@ -1,12 +1,13 @@
+using Amendment.Model.DataModel;
 using Amendment.Server.Extensions;
 using Amendment.Server.Mediator.Commands;
 using Amendment.Shared.Requests;
 using Amendment.Shared.Responses;
-using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Mapster;
 
 namespace Amendment.Server.Controllers
 {
@@ -25,8 +26,7 @@ namespace Amendment.Server.Controllers
         [AllowAnonymous]
         public async Task<IResult> Login([FromBody] AccountLoginRequest userForAuthentication)
         {
-            var command = new AccountLoginCommand
-                { Username = userForAuthentication.Username, Password = userForAuthentication.Password };
+            var command = userForAuthentication.Adapt<AccountLoginCommand>();
             var result = await _mediator.Send(command);
             return result.ToResult();
         }
