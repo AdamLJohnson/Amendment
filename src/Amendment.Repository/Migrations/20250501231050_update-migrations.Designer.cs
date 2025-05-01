205661459509b616e39b2f923c3e92e870e482a0
Adam Johnson <columbus@adamlj.com>
2025-05-01 16:14:29 -0700

    Nuget Updates

--- src/Amendment.Repository/Migrations/20250501231050_update-migrations.Designer.cs
+++ src/Amendment.Repository/Migrations/20250501231050_update-migrations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Amendment.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Amendment.Repository.Migrations
 {
     [DbContext(typeof(AmendmentContext))]
-    partial class AmendmentContextModelSnapshot : ModelSnapshot
+    [Migration("20250501231050_update-migrations")]
+    partial class updatemigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Amendment.Repository/Migrations/20250501231050_update-migrations.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Amendment.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatemigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "UserRoles",
+                columns: new[] { "RolesId", "UserId" },
+                values: new object[] { 5, 1 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "UserRoles",
+                keyColumns: new[] { "RolesId", "UserId" },
+                keyValues: new object[] { 5, 1 });
+        }
+    }
+}
